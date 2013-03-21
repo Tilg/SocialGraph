16195fc9187b3fecc1dbfb4e5fbc20b0d654d97b
Omar Eddasser <omar.eddasser@gmail.com>
2013-03-21 13:46:15 +0100

    Parseur de la ligne de commande (classe main) + graph.txt

---
+++ src/graph/Search.java
@@ -0,0 +1,11 @@
+package graph;
+
+/**
+ * This type is useful for describing the strategy for searching in a graph
+ * 
+ * @author Omar
+ * @version 0.1
+ */
+public enum Search{
+	DEPTH_FIRST, BREADTH_FIRST;
+};
---
+++ src/parser/CommandLineParser.java
@@ -0,0 +1,24 @@
+package parser;
+
+import java.util.Scanner;
+
+import com.sun.corba.se.impl.orbutil.graph.Graph;
+
+/**
+ * @author omar
+ * @version 0.1
+ */
+public class CommandLineParser{
+	private final static String REQUEST_QUIT = "quit";
+	private Graph graph;
+	private static Scanner scanner = new Scanner(System.in);
+	
+	public CommandLineParser(String[] args){
+		
+	}
+	
+	public static void main(String[] args){
+		
+	}
+	
+}
